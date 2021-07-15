0c9d26111d77110dd9ca6e883422ce4fe5d408d9
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-15 20:53:37 +0530

    [Ravina] Add : Welcome to Line Comparision Computation Program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LineComparisionUsingOOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program ");
+            Console.WriteLine(".................................................");
+
+            //UC_1.CalculateLength();
+           // UC_2.CompareTheLength();
+           // UC_3.CompareTheLength();
+            
+            UC_4 uc4 = new UC_4();
+            uc4.EnterInput();
+            uc4.CalculateTheLength();
+            uc4.CompareThelength();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
